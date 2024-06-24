42bdc13b94dad9dae31a1b960833902856b66c01
OksanaFedyukova <avseniya22@gmail.com>
2024-06-24 16:49:28 +0200

    fixed ports

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+require('dotenv').config();
 
 const {
   ApolloServerPluginDrainHttpServer,
