67db254bbc527c783629f68cc239b1894118fc93
OksanaFedyukova <avseniya22@gmail.com>
2024-06-26 20:08:53 +0200

    new build

--- client/src/components/MovieCard/index.jsx
+++ client/src/components/MovieCard/index.jsx
@@ -10,21 +10,7 @@ import { Box } from '@mui/material';
 
 export default function MovieCard({movie, onCardSelect}) {
 
-  const formatrelease_date = (dateString) => {
-    if (!dateString) return ''; 
   
-    const parts = dateString.split(/[T\s]/);
-    if (parts.length < 1) return ''; 
-  
-    const [year, month] = parts[0].split('-').slice(0, 2);
-  
-    const monthNames = [
-      'January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'
-    ];
-  
-    return `${monthNames[parseInt(month, 10) - 1]} ${year}`;
-  };
   
   const renderStars = () => {
     const starCount = Math.round(movie.voteAverage / 2); 
