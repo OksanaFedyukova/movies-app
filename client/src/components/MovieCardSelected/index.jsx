c6e4d5a98680ab6bcca24777d415f6d8b0e31812
OksanaFedyukova <avseniya22@gmail.com>
2024-07-02 22:41:16 +0200

    +fixed card

--- client/src/components/MovieCardSelected/index.jsx
+++ client/src/components/MovieCardSelected/index.jsx
@@ -1,24 +1,14 @@
-import {useState} from 'react'
 import PropTypes from 'prop-types';
 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Box, IconButton, Menu, MenuItem } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Box, IconButton } from '@mui/material';
 import StarsIcon from '@mui/icons-material/Stars';
-
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 export default function MovieCardSelected({movie, onCardDelete}) {
-  const [anchorEl, setAnchorEl] = useState(null);
 
-  const handleMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
   const renderStars = () => {
     const starCount = Math.round(movie.voteAverage / 2); 
     const stars = Array.from({ length: 5 }, (_, index) => (
@@ -27,55 +17,42 @@ export default function MovieCardSelected({movie, onCardDelete}) {
     return stars;
   };
   return (
-    <Card sx={{ display: 'flex', width:"90%", ml:2, mt:2 }}>
-     
-   <Menu
-        id="menu-appbar"
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={Boolean(anchorEl)}
-        onClose={handleMenuClose}
-      >
-        <MenuItem onClick={onCardDelete}>Delete</MenuItem>
-       
-      </Menu>
-      
-         <CardMedia
+    <Card sx={{ display: 'flex', width: "90%", ml: 2, mt: 2, justifyContent: 'space-between' }}>
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <CardMedia
         component="img"
-        sx={{ height: 130, minWidth: 150, maxWidth:150}}
+        sx={{ height: 130, minWidth: 150, maxWidth: 150 }}
         image={movie.image}
         alt={movie.title}
       />
-     <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="subtitle1" variant="h6" 
-          sx={{
-                    fontSize:15,
-                }}>
-           {movie.title}
-          </Typography>
-          {movie.genres?.length ? (
-             <Typography variant="subtitle1" color="text.secondary" component="div">
-           {movie.genres[0].name} 
-           </Typography>) : null}
-         
-           <Typography  variant="h5">
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        {renderStars()}
+      <CardContent sx={{ flex: '1 0 auto' }}>
+      <Typography variant="p" sx={{ 
+        fontWeight: 700, 
+        textWrap:"wrap" }}>
+
+          {movie.title}
+        </Typography>
+        {movie.genres?.length ? (
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            {movie.genres[0].name}
+          </Typography>) : null}
+        <Typography variant="h5">
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            {renderStars()}
+          </Box>
+        </Typography>
+      </CardContent>
+    </Box>
+
+    <Box sx={{ position: 'relative' }}>
+      <Box sx={{ position: 'absolute', top: 0, right: 0 }}>
+        <IconButton aria-label="delete" onClick={onCardDelete}>
+          <DeleteForeverIcon />
+        </IconButton>
       </Box>
-    </Typography>
-        </CardContent>
-        <IconButton aria-label="moreOptions" sx={{ color: 'black', borderRadius:"3px" }} onClick={handleMenuOpen}>  
-             <MoreVertIcon  onClick={onCardDelete}sx={{ color: 'black' }}/>         
-          </IconButton>
-    </Card>
+    </Box>
+
+  </Card>
   );
 }
 MovieCardSelected.propTypes = {
